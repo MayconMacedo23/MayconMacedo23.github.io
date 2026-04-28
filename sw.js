465b83ddbaf2e8a4523945194656c7c997a425c9
Maycon D. <127693075+MayconMacedo23@users.noreply.github.com>
2026-04-28 10:41:29 -0300

    Update sw.js
    teste

--- sw.js
+++ sw.js
@@ -1,1 +1,29 @@
-self.addEventListener('fetch', () => {});
+const CACHE_NAME = 'spiltag-inventario-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png'
+];
+
+// Instalando o Service Worker e fazendo cache dos arquivos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Interceptando as requisições (Faz o app abrir mesmo sem internet)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retorna o arquivo do cache se existir, senão busca na rede
+        return response || fetch(event.request);
+      })
+  );
+});
